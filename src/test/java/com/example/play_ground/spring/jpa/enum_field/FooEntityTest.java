79727c3b0486501cbd714f080622470ff4ac6517
rohminji <balminji0106@naver.com>
2024-02-05 02:53:34 +0900

    Simple Batch Project

---
+++ src/test/java/com/example/play_ground/spring/jpa/enum_field/FooEntityTest.java
@@ -0,0 +1,14 @@
+package com.example.play_ground.spring.jpa.enum_field;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+
+
+class FooEntityTest {
+    @Test
+    void enumPrac() {
+        Color color = null;
+        assertThatThrownBy(() -> color.getName()).isInstanceOf(NullPointerException.class);
+    }
+}
